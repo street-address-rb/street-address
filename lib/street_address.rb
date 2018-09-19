8e6941474d8a7aa01228582f34c9352319467304
Christoph Wagner <christoph@wrstudios.com>
2018-09-18 17:12:57 -0700

    StreetAddress.parse uses all parsers and returns longest match

--- lib/street_address.rb
+++ lib/street_address.rb
@@ -4,7 +4,9 @@ require 'street_address/us'
 require 'street_address/ca'
 
 module StreetAddress
+  Parsers = [US, CA]
+
   def self.parse(location, args={})
-    return US.parse(location, args) || CA.parse(location, args)
+    return Parsers.map { |klass| klass.parse(location, args) }.compact.max
   end
 end
